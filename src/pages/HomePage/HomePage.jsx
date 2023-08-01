ce8e6baae943a384a20c447c59536ba113218ce4
Cole-B-Johnson <cole@levitree.com>
2023-08-02 00:08:42 +0100

    temporary removal of header bar

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import './HomePage.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Outlet } from 'react-router-dom';
-import NavBarComponent from '../../components/NavbarComponent/NavbarComponent';
+// import NavBarComponent from '../../components/NavbarComponent/NavbarComponent';
 
 
 const HomePage = () => {
     return (
         <div className='homepage_container'>
-            <NavBarComponent/>
+            {/* <NavBarComponent/> */}
             {/* Outlet is the output of all the children as per the routes */}
             <Outlet/>
         </div>
